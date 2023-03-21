ce25946eb450d2300ab7d26ab2b841ccb51459b0
nhunht <nhtn0311@gmail.com>
2023-03-21 20:49:38 +0700

    Assingment 3

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,36 +1,37 @@
-const Players = require('../models/player');
+const Players = require("../models/player");
 
 class IndexController {
-    index(req, res, next) {
-        let pageSize = req.query.pageSize || 6;
-        let pageIndex = req.query.pageIndex || 1;
+  index(req, res, next) {
+    let pageSize = req.query.pageSize || 6;
+    let pageIndex = req.query.pageIndex || 1;
 
-        Players
-            .aggregate([{
-                $lookup: {
-                    from: 'nations',
-                    localField: 'nationId',
-                    foreignField: '_id',
-                    as: 'nation'
-                }
-            }])
-            .skip((pageIndex - 1) * pageSize)
-            .limit(pageSize)
-            .exec((err, players) => {
-                Players.countDocuments((err, count) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.render('index', {
-                        title: 'Home Page',
-                        players: players,
-                        button: req.isAuthenticated() ? 'Logout' : 'Login',
-                        count: Math.ceil(count / pageSize),
-                        pageIndex: pageIndex,
-                    })
-                })
-            })
-    }
+    Players.aggregate([
+      {
+        $lookup: {
+          from: "nations",
+          localField: "nationId",
+          foreignField: "_id",
+          as: "nation",
+        },
+      },
+    ])
+      .skip((pageIndex - 1) * pageSize)
+      .limit(pageSize)
+      .exec((err, players) => {
+        Players.countDocuments((err, count) => {
+          if (err) {
+            return next(err);
+          }
+          res.render("index", {
+            title: "Home Page",
+            players: players,
+            button: req.isAuthenticated() ? "Logout" : "Login",
+            count: Math.ceil(count / pageSize),
+            pageIndex: pageIndex,
+          });
+        });
+      });
+  }
 }
 
-module.exports = new IndexController;
+module.exports = new IndexController();
--- controllers/nationController.js
+++ controllers/nationController.js
@@ -1,64 +1,84 @@
-const Nations = require('../models/nation');
-const isoCountries = require('i18n-iso-countries');
-isoCountries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+const Nations = require("../models/nation");
+const isoCountries = require("i18n-iso-countries");
+isoCountries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 class NationController {
-    index(req, res, next) {
-        Nations.find({})
-            .then((nations) => {
-                res.render('nations', {
-                    title: 'Nations',
-                    nations: nations,
-                    button: req.isAuthenticated() ? 'Logout' : 'Login',
-                    isAdmin: req.user.isAdmin ? '' : 'hidden'
-                });
-            }).catch(next);
-    }
+  index(req, res, next) {
+    let pageSize = req.query.pageSize || 10;
+    let pageIndex = req.query.pageIndex || 1;
 
-    create(req, res, next) {
-        const nation = new Nations(req.body);
-        nation.ensign = 'https://flagcdn.com/160x120/' + isoCountries.getAlpha2Code(nation.name, 'en').toLowerCase() + '.png';
-        nation.save()
-            .then(() => res.redirect('/nations'))
-            .catch(error => {});
-    }
+    Nations.find({})
+      .skip((pageIndex - 1) * pageSize)
+      .limit(pageSize)
+      .exec((err, nations) => {
+        Nations.countDocuments((err, count) => {
+          if (err) {
+            return next(err);
+          }
+          res.render("nations", {
+            title: "Nations",
+            nations: nations,
+            button: req.isAuthenticated() ? "Logout" : "Login",
+            isAdmin: req.user.isAdmin ? "" : "hidden",
+            count: Math.ceil(count / pageSize),
+            pageIndex: pageIndex,
+          });
+        });
+      });
+  }
 
-    formEdit(req, res, next) {
-        const nationId = req.params.nationId;
-        Nations.findById(nationId)
-            .then((nation) => {
-                res.render('editNation', {
-                    title: 'The detail of Nation',
-                    nation: nation
-                })
-            })
-            .catch(next);
-    }
+  create(req, res, next) {
+    const nation = new Nations(req.body);
+    nation.ensign =
+      "https://flagcdn.com/160x120/" +
+      isoCountries.getAlpha2Code(nation.name, "en").toLowerCase() +
+      ".png";
+    nation
+      .save()
+      .then(() => res.redirect("/nations"))
+      .catch((error) => {});
+  }
 
-    edit(req, res, next) {
-        var code = isoCountries.getAlpha2Code(req.body.name, 'en').toLowerCase();
-        req.body.ensign = 'https://flagcdn.com/160x120/' + code + '.png';
-        Nations.updateOne({
-                _id: req.params.nationId
-            }, req.body)
-            .then(() => {
-                res.redirect('/nations')
-            })
-            .catch(next)
-    }
+  formEdit(req, res, next) {
+    const nationId = req.params.nationId;
+    Nations.findById(nationId)
+      .then((nation) => {
+        res.render("editNation", {
+          title: "The detail of Nation",
+          nation: nation,
+        });
+      })
+      .catch(next);
+  }
 
-    delete(req, res, next) {
-        Nations.deleteOne({
-                _id: req.params.nationId
-            }, req.body)
-            .then(() => {
-                res.redirect('/nations')
-            })
-            .catch(next)
-    }
+  edit(req, res, next) {
+    var code = isoCountries.getAlpha2Code(req.body.name, "en").toLowerCase();
+    req.body.ensign = "https://flagcdn.com/160x120/" + code + ".png";
+    Nations.updateOne(
+      {
+        _id: req.params.nationId,
+      },
+      req.body
+    )
+      .then(() => {
+        res.redirect("/nations");
+      })
+      .catch(next);
+  }
 
-    paged(req, res, next) {
+  delete(req, res, next) {
+    Nations.deleteOne(
+      {
+        _id: req.params.nationId,
+      },
+      req.body
+    )
+      .then(() => {
+        res.redirect("/nations");
+      })
+      .catch(next);
+  }
 
-    }
+  paged(req, res, next) {}
 }
-module.exports = new NationController;
+module.exports = new NationController();
